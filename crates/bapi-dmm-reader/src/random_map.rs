fe4c305ebb26981f928be7a5661dd4658610bc34
tigercat2000 <nick.pilant2@gmail.com>
2024-08-01 17:38:08 -0700

    Add random_map automata generator

--- crates/bapi-dmm-reader/src/random_map.rs
+++ crates/bapi-dmm-reader/src/random_map.rs
@@ -1,1 +1,76 @@
-//! This file implements [Baystation12](https://github.com/Baystation12/Baystation12)'s random map generation
+use byondapi::{prelude::*, value::ByondValue};
+use rand::{distributions::Bernoulli, prelude::Distribution};
+
+use crate::_compat::setup_panic_handler;
+
+const CELL_THRESHOLD: usize = 5;
+
+#[byondapi::bind]
+pub fn bapidmm_generate_automata(
+    limit_x: ByondValue,
+    limit_y: ByondValue,
+    iterations: ByondValue,
+    initial_wall_cell: ByondValue,
+) {
+    setup_panic_handler();
+
+    let limit_x = limit_x.get_number()? as usize;
+    let limit_y = limit_y.get_number()? as usize;
+    let iterations = iterations.get_number()? as usize;
+    let initial_wall_cell = initial_wall_cell.get_number()? as usize;
+
+    let mut map = seed_map(limit_x, limit_y, initial_wall_cell)?;
+    for _ in 1..iterations {
+        map = map
+            .iter()
+            .enumerate()
+            .map(|(i, _)| {
+                let mut count = 0;
+
+                for x in [-1, 0, 1] {
+                    for y in [-1, 0, 1] {
+                        let idx = ((i as i32) + x + (y * (limit_y as i32))) as usize;
+
+                        if let Some(&value) = map.get(idx) {
+                            let is_border_left = idx % limit_x == 0;
+                            let is_border_right = (idx + 1) % limit_x == 0;
+
+                            if is_border_left && x == -1 {
+                                continue;
+                            }
+                            if is_border_right && x == 1 {
+                                continue;
+                            }
+
+                            if value {
+                                count += 1;
+                            }
+                        }
+                    }
+                }
+
+                count >= CELL_THRESHOLD
+            })
+            .collect()
+    }
+
+    let byond_list: Vec<ByondValue> = map
+        .iter()
+        .map(|&b| ByondValue::new_num(if b { 1. } else { 0. }))
+        .collect();
+
+    Ok(ByondValue::try_from(byond_list.as_slice())?)
+}
+
+fn seed_map(limit_x: usize, limit_y: usize, percent_chance: usize) -> eyre::Result<Vec<bool>> {
+    let mut map = vec![false; limit_x * limit_y];
+
+    let d = Bernoulli::new(percent_chance as f64 / 100.0)?;
+
+    map = map
+        .iter()
+        .map(|_| d.sample(&mut rand::thread_rng()))
+        .collect();
+
+    Ok(map)
+}
